c43193f67b02d99f8102c489842923bf25d7af70
平澤 孝行 <hrsweng0410@gmail.com>
2021-04-29 19:18:43 +0900

    add Chessboard

---
+++ LearningJava/AOJ/Chessboard.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Chessboard {
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        for (int i = 1; i > 0; i++){
+            int H = sc.nextInt();
+            int W = sc.nextInt();
+            if (H == 0 && W == 0) {
+                break;
+            }else {
+                for (int j = 1; j <= H; j++) {
+
+                    for ( int k = 1; k<= W; k++) {
+                    
+                        if(k == W) {
+                            if (k % 2 == 0) {
+                                System.out.print(".");
+                                System.out.println("");
+                            }else {
+                                System.out.print("#");
+                                System.out.println("");
+                            }
+                        }else {
+                            if (k % 2 == 0){
+                                System.out.print(".");
+                            }else {
+                                System.out.print("#");
+                            }
+                            
+                            
+                        }
+                    
+                    }
+                
+                if ( j == H) {
+                    System.out.println("");
+                }
+                
+                }
+            }   
+            
+            
+        }
+
+    }
+
+}
